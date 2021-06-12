f6f4b00e51c5ad62bd8cfa8124ea7a20db2b4f80
Chris Wong <ss321324@gmail.com>
2021-06-12 21:28:45 +0800

    Update news detail

--- js/pages/NewsDetailComponent.vue
+++ js/pages/NewsDetailComponent.vue
@@ -16,14 +16,7 @@
 export default {
   data() {
     return {
-      news: {
-        title: "Title1",
-        subtitle: "Sub1",
-        photo: "/media/news/d98d56d1-f203-4ed1-9e85-1f371ca795c8.png",
-        text: "asdfasbn das das\r\ns acascasca",
-        content:
-          "asdasujb fjkawbfjcvksdn  c  <br>asc as<br>cas<br>edfnkjgnvjowencvpasmcas<br>csdnjvberjkolnvmsd,vsd",
-      },
+      news: {},
     };
   },
   created() {
@@ -31,7 +24,7 @@ export default {
     axios
       .get("https://hkg.games/api/news_detail/" + this.$route.params.id)
       .then((resp) => {
-        this.esports = resp;
+        this.news = resp.data;
       })
       .finally(() => {
         this.loading = false;
